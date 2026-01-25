c6f0e4b78f2a2169ae0467b2b73573042d389694
Annazak24 <zaqaryan.anna856@gmail.com>
2026-01-25 21:38:06 +0400

    Homework 4

--- src/main/java/factory/WebDriverFactory.java
+++ src/main/java/factory/WebDriverFactory.java
@@ -1,27 +1,35 @@
 package factory;
 
-import exceptions.BrowserNotSupportedException;
 import factory.settings.ChromeSettings;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WebDriverFactory {
 
     private final String runType = System.getProperty("run.type", "remote");
 
     public WebDriver create() {
-        System.out.println("DOCKER_API_VERSION = " + System.getenv("DOCKER_API_VERSION"));
         try {
             if ("remote".equals(runType)) {
                 ChromeOptions options = new ChromeOptions();
+
                 options.setCapability("browserName", "chrome");
                 options.setCapability("browserVersion", "128.0");
-                options.setCapability("selenoid:options", java.util.Map.of(
-                        "enableVNC", true
-                ));
+                Map<String, Object> selenoidOptions = new HashMap<>();
+                selenoidOptions.put("enableVNC", true);
+                selenoidOptions.put("sessionTimeout", "15m");
+                options.setCapability("selenoid:options", selenoidOptions);
+
+                Map<String, String> mobileEmulation = new HashMap<>();
+                mobileEmulation.put("deviceName", "Nexus 5");
+                options.setExperimentalOption("mobileEmulation", mobileEmulation);
+
 
                 return new RemoteWebDriver(
                         new URL("http://localhost:4444/wd/hub"),
@@ -29,14 +37,10 @@ public class WebDriverFactory {
                 );
             }
 
-            // LOCAL
-            return new org.openqa.selenium.chrome.ChromeDriver(
-                    (ChromeOptions) new ChromeSettings().settings()
-            );
+            return new ChromeDriver((ChromeOptions) new ChromeSettings().settings());
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Չհաջողվեց ստեղծել WebDriver-ը: " + e.getMessage(), e);
         }
-
     }
 }
